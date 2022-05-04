9d4c85d2b519db7e1cba2cebf4799c10461bb066
Sherif Nafee <shico.m.nafee@gmail.com>
2022-05-05 01:07:31 +0300

    Make troops attack

--- Assets/Scripts/Units/Troop.cs
+++ Assets/Scripts/Units/Troop.cs
@@ -7,8 +7,9 @@ using UnityEngine.AI;
 public class Troop : MonoBehaviour
 {
     [SerializeField] protected int _health = 100;
-    public Transform Target;
-    public Action TroopDied;
+    [SerializeField] protected float _attackRange = 2.0f;
+    [SerializeField] protected float _attackInterval = 2.0f;
+    [SerializeField] protected int _attackValue = 20;
     public Action<Troop> TroopReady;
     public List<Troop> Enemies;
 
@@ -23,17 +24,19 @@ public class Troop : MonoBehaviour
 
     public void Play()
     {
-        _agent.speed = _originalSpeed;
-        // TODO : enable attacking
+        _agent.isStopped = false;
+        _paused = false;
+        _attacking = true;
     }
 
     public void Pause()
     {
-        _agent.speed = 0;
-        //TODO : disable attacking
+        _agent.isStopped = true;
+        _paused = true;
+        _attacking = false;
     }
-    
-    
+
+
     public void FindNewTarget()
     {
         Troop target = null;
@@ -41,7 +44,6 @@ public class Troop : MonoBehaviour
         foreach (var troop in Enemies)
         {
             if (troop == null) continue;
-
             Vector3 distance = troop.transform.position - transform.position;
             if (distance.magnitude < minDistance)
             {
@@ -49,22 +51,41 @@ public class Troop : MonoBehaviour
                 target = troop;
             }
         }
+
         if (target != null) SetTarget(target);
     }
 
     public void SetTarget(Troop target)
     {
-        target.TroopDied += TargetDied;
-        StartCoroutine(FollowTarget(1f, target.transform));
+        _target = target;
+        StartCoroutine(FollowTarget(target.transform));
     }
-    
-    protected IEnumerator FollowTarget(float range, Transform target) {
+
+    protected IEnumerator FollowTarget(Transform target)
+    {
         Vector3 previousTargetPosition = new Vector3(float.PositiveInfinity, float.PositiveInfinity);
         _lockedOnTarget = true;
-        while (_lockedOnTarget) {
-            if (Vector3.SqrMagnitude (previousTargetPosition - target.position) > range) {
-                _agent.SetDestination (target.position);
-                previousTargetPosition = target.position;
+        while (_lockedOnTarget)
+        {
+            if (target == null)
+            {
+                _agent.ResetPath();
+                FindNewTarget();
+                break;
+            }
+
+            if (Vector3.SqrMagnitude(transform.position - target.position) >= _attackRange)
+            {
+                if (_agent.isStopped && !_paused) _agent.isStopped = false;
+                if (Vector3.SqrMagnitude(previousTargetPosition - target.position) >= 0.5f)
+                {
+                    _agent.SetDestination(target.position);
+                    previousTargetPosition = target.position;
+                }
+            }
+            else
+            {
+                _agent.isStopped = true;
             }
 
             if (_agent.pathStatus == NavMeshPathStatus.PathComplete && !_signaledReady)
@@ -72,32 +93,46 @@ public class Troop : MonoBehaviour
                 TroopReady?.Invoke(this);
                 _signaledReady = true;
             }
-            yield return new WaitForSeconds (0.7f);
+
+            yield return new WaitForSeconds(0.7f);
         }
+
         yield return null;
     }
-    
+
+    private void Update()
+    {
+        if (_attackWait < _attackInterval) _attackWait += Time.deltaTime;
+        if (_target == null || !_attacking) return;
+        if (Vector3.SqrMagnitude(transform.position - _target.transform.position) <= _attackRange)
+        {
+            if (_attackWait < _attackInterval) return;
+            Attack();
+            _attackWait = 0;
+        }
+    }
+
+    private void Attack()
+    {
+        _target.TakeDamage(_attackValue);
+    }
+
     private void Die()
     {
-        TroopDied?.Invoke();
         Destroy(gameObject);
     }
 
-    private void TargetDied()
-    {
-        _lockedOnTarget = false;
-        FindNewTarget();
-    }
-    
     private void OnEnable()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _originalSpeed = _agent.speed;
     }
 
-    
+
     protected NavMeshAgent _agent;
+    public Troop _target;
     private bool _lockedOnTarget;
     private bool _signaledReady;
-    private float _originalSpeed;
+    private float _attackWait;
+    private bool _attacking = true;
+    private bool _paused = false;
 }
