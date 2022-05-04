82d042a3dad289f2a4e68ff68cca95dd42284dd2
Sherif Nafee <shico.m.nafee@gmail.com>
2022-05-04 08:30:49 +0300

    Create battle controller

---
+++ Assets/Scripts/BattleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class BattleController
+{
+    public List<Troop> FirstArmy;
+    public List<Troop> SecondArmy;
+
+    public void Play()
+    {
+        foreach (var troop in FirstArmy)
+        {
+            troop.Play();
+        }
+        foreach (var troop in SecondArmy)
+        {
+            troop.Play();
+        }
+    }
+
+    public void Pause()
+    {
+        foreach (var troop in FirstArmy)
+        {
+            troop.Pause();
+        }
+        foreach (var troop in SecondArmy)
+        {
+            troop.Pause();
+        }
+    }
+}
